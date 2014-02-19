a63812c97a739413de6d651119e3b31b93685c9b
jkarpins <jkarpins@uhura.emi.u-bordeaux1.fr>
2014-02-19 14:40:50 +0100

    Validation de la synchronisation.

--- MyTodo/src/model/SynchronisationForm.java
+++ MyTodo/src/model/SynchronisationForm.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 
 import com.google.gdata.client.calendar.CalendarService;
 import com.google.gdata.data.DateTime;
 import com.google.gdata.data.PlainTextConstruct;
 import com.google.gdata.data.calendar.CalendarEventEntry;
+import com.google.gdata.data.calendar.CalendarEventFeed;
 import com.google.gdata.data.extensions.When;
 import com.util.HibernateUtil;
 
@@ -21,7 +23,7 @@ public class SynchronisationForm {
 
 	public final static String CALENDAR_PARAM =  "exampleCo-exampleApp-1.0";
 	public final static String CALENDAR_KEY = "mytodo";
-	
+
 	public void synchro( HttpServletRequest request, Utilisateurs utilisateurs )
 	{
 		try {
@@ -44,31 +46,44 @@ public class SynchronisationForm {
 
 
 		CalendarService myService = new CalendarService(CALENDAR_PARAM);
-		myService.setUserCredentials(utilisateur.getGmail(), utilisateur.getGmotDePasse());
+		myService.setUserCredentials(utilisateur.getGmail(), utilisateur.getGmotDePasse());	
 
 		URL postURL = new URL("http://www.google.com/calendar/feeds/" + utilisateur.getGmail() + "/private/full");		
+
+		CalendarEventFeed myFeed = myService.getFeed(postURL, CalendarEventFeed.class);
 
 		for(Todo t : ListerTodo)
 		{
 			CalendarEventEntry myEvent = new CalendarEventEntry();
-			//Set the title and description
+
 			myEvent.setTitle(new PlainTextConstruct(t.getTitre()));
 			myEvent.setContent(new PlainTextConstruct(t.getDescription()));
-			myEvent.setId(CALENDAR_KEY+t.getIdTodo());
 
-			System.out.println(t.getEcheanceBegin());
-			System.out.println(t.getEcheanceEnd());
-			
 			DateTime startTime = new DateTime(t.getEcheanceBegin());
 			DateTime endTime = new DateTime(t.getEcheanceEnd());
-			//DateTime startTime = DateTime.parseDateTime("2014-02-17T17:00:00-08:00");
-			//DateTime endTime = DateTime.parseDateTime("2014-02-18T17:00:00-08:00");
+
 			When eventTimes = new When();
 			eventTimes.setStartTime(startTime);
 			eventTimes.setEndTime(endTime);
 			myEvent.addTime(eventTimes);
 
-			myService.insert(postURL, myEvent);
+
+			boolean exist = false;
+			for(int i=0; i<myFeed.getEntries().size(); i++)
+			{
+				CalendarEventEntry matchEntry = (CalendarEventEntry)
+						myFeed.getEntries().get(i);
+				if(matchEntry.getId().equals(t.getIdCalendar()))
+					exist = true;
+			}		
+			if(!exist)
+			{
+				CalendarEventEntry lastMatchEntry = myService.insert(postURL, myEvent);		
+				String query = "UPDATE Todo SET ID_Calendar = '"+ lastMatchEntry.getId() +"' WHERE ID_Todo = '" + t.getIdTodo() + "' AND FK_ID_Utilisateur = '"+ utilisateur.getIdUtilisateur() + "'";
+				SQLQuery sqlQuery = session.createSQLQuery(query);
+				sqlQuery.executeUpdate();
+
+			}
 		}
 
 	}
